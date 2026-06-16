6bd0f898ccbafd9d59cde04a3a9b6ba5c690348d
Paul <phack@student.tgm.ac.at>
2026-06-16 10:33:25 +0200

    Initial commit

---
+++ src/main/java/io/github/sossegruss/minecraftSkillSystem/MinecraftSkillSystem.java
@@ -0,0 +1,16 @@
+package io.github.sossegruss.minecraftSkillSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftSkillSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
